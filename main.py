5d2b839973f4d6ac9025efac65d289a4cd147430
Aras-Cemal <152717416+Aras-Cemal@users.noreply.github.com>
2023-12-05 19:40:04 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+a = { 'kaçta uyanırsın':'saat 7 de uyanırım',
+'kaçta yatarsın':'saat on bir buçukta yatarım',
+'hangi kurslardan geçtin':'python basic ve şu anda python proya başladım'
+
+    
+
+}
+
+
+print(a['hangi kurslardan geçtin'])
